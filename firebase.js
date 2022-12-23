4a09fa5d8958dfd1ac6a8112d7f3d9fbff0f91a0
sorinupwork <sorin.upwork@gmail.com>
2022-12-23 16:26:35 +0200

    add loginScreen

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCklvs5eG3-BehX7dN_ekmrNNSo86hH3Lg",
+  authDomain: "signal-clone-bf75e.firebaseapp.com",
+  projectId: "signal-clone-bf75e",
+  storageBucket: "signal-clone-bf75e.appspot.com",
+  messagingSenderId: "423542260401",
+  appId: "1:423542260401:web:16ce8bf5cc9e354a531890",
+};
---
+++ screens/LoginScreen.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View, KeyboardAvoidingView } from "react-native";
+import React, { useState } from "react";
+import { Button, Image, Input } from "@rneui/themed";
+import { StatusBar } from "expo-status-bar";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+
+      <Image
+        source={{ uri: "https://i.ibb.co/SN1jYxv/live-chat.png" }}
+        style={{ width: 200, height: 200 }}
+      />
+
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button title="Login" onPress={signIn} containerStyle={styles.button} />
+      <Button title="Register" type="outline" containerStyle={styles.button} />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
